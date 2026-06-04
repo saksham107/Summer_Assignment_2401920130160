61e08d1a455c56fdf744f1901084d3258b427c7a
Saksham Kumar <sakshamkumar3413@gmail.com>
2026-06-04 19:35:18 +0530

    Create Matrix_Diagonal_Sum.cpp

---
+++ Week_1/Day_4/Matrix_Diagonal_Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        //int j=mat[0].size();
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum=sum+mat[i][i];
+            sum += mat[i][n-i-1] ;
+            if(i==n-i-1){
+                sum -= mat[i][i];
+            }
+
+        }
+        return sum;
+    }
+};
